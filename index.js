7cb68cb6bfffe63aa2f40b92194e59a6d276820c
hanif365 <m.a.hanifkhan64@gmail.com>
2021-04-30 12:58:42 +0600

    comment added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwfp8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
 
-// Middleware
+// Middleware start
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
